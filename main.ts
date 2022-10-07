558fd54ed9d8b8035bfa6080161a2f5f34a6d2f9
karolruizq <110991486+karolruizq@users.noreply.github.com>
2022-10-06 19:03:21 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,21 @@
+input.onGesture(Gesture.LogoUp, function () {
+    basic.showIcon(IconNames.Angry)
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    basic.showIcon(IconNames.Asleep)
+})
+input.onGesture(Gesture.ScreenUp, function () {
+    basic.showIcon(IconNames.Meh)
+})
+input.onGesture(Gesture.ScreenDown, function () {
+    basic.showIcon(IconNames.Surprised)
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showIcon(IconNames.Ghost)
+})
+input.onGesture(Gesture.TiltRight, function () {
+    basic.showIcon(IconNames.Silly)
+})
+input.onGesture(Gesture.LogoDown, function () {
+    basic.showIcon(IconNames.Skull)
+})
